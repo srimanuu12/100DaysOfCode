bd8614dd9ab7cf3d40e5ef18abeb36d299232ba4
manuu <admin@django.com>
2022-08-09 15:38:48 +0530

    Tip calculator created and working well

--- Day2/tipcalculator.py
+++ Day2/tipcalculator.py
@@ -1,1 +1,9 @@
+print("Welcome to Tip Calculator")
 
+amount = float(input("Enter amount: "))
+people = int(input("How many members? "))
+tip = int(input("Please opt tip 10, 15, 20:" ))
+tip_amount = amount * (tip/100)
+total_amount = amount + tip_amount
+each_person = total_amount/people
+print(f"Each person has to pay: {each_person}")
