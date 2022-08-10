d7e4091640dfc400bdd32f51375e0d7dcff7c767
manuu <admin@django.com>
2022-08-11 00:04:14 +0530

    Caesar Cipher: Encrypt and Decrypt

---
+++ Day8/README.md
@@ -0,0 +1,4 @@
+# Caesar Cipher challenge
+
+By using old encryption method, encode and decode using Functions
+
--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,1 +1,29 @@
+import random
 
+def cipher():
+    def caesar(choice,msg,shift_num):
+        alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'X', 'W', 'Y', 'Z']
+        en_msg = ""
+        for i in msg:
+            if choice == "decode":
+                en_msg += alpha[(alpha.index(i)-shift_num) % len(alpha)]
+            else:
+                en_msg += alpha[(alpha.index(i)+shift_num) % len(alpha)]
+        return en_msg
+
+    choice = input("What do you want to do? Type 'encode' - encrypt and 'decode' - decrypt:  ")
+    msg = input("Enter the message: ")
+    shift_num = random.choice(range(1,52))
+    print(f"{choice}ed result is {caesar(choice,msg,shift_num)}")
+    
+    print("Do you want to try again? Type 'Yes' or 'No': ",end=" ")
+    again = input().lower()
+
+    if again == "yes":
+        cipher()
+    elif again == "no":
+        print("Thank you! Try later")
+    else:
+        print("Invalid Input")
+
+cipher()
