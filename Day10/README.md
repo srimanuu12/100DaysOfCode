f1e8311af78eb005f6e4162848bf403e1bf4b99d
manuu <admin@django.com>
2022-08-19 22:28:33 +0530

    Calculator using functions

---
+++ Day10/README.md
@@ -0,0 +1,3 @@
+# Calculator
+
+A calculator that takes the number and operation information from user and provides the result.
--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,1 +1,51 @@
+def cal(choice,n1,n2):
+    if choice == "+":
+        return n1 + n2
+    elif choice == "-":
+        return n1 - n2
+    elif choice == "*":
+        return n1 * n2
+    else:
+        return n1 / n2
+
+
+def calculator():
+    logo = '''
+ _____________________
+|  _________________  |
+| |              0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+'''
+    print(logo)
+    again = True
+    n1 = float(input("Enter first number: "))
+    while again:        
+        print("+\n-\n*\n/")
+        choice = input("Pick an operator: ")
+        n2 = float(input("Enter next number: "))
+        ans = cal(choice,n1,n2)
+        print(f"{n1} {choice} {n2} = {ans}")
+
+        again = input(f"Type 'y' to continue calculation with {ans} or Type 'n' to start new calculation: ").lower()
+
+        if again == "y":
+            n1 = ans
+            again = True
+
+        elif again == "n":
+            calculator()
+        else:
+            print("Invalid input")
+
+calculator()
 
