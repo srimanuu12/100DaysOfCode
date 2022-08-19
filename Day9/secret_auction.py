b7b57c748d997f0b0327084a5f10c4dee346c5e4
manuu <admin@django.com>
2022-08-19 17:16:19 +0530

    Secret Auction

--- Day9/secret_auction.py
+++ Day9/secret_auction.py
@@ -1,1 +1,53 @@
+def info():
+
+    def key(dic,count):
+        for k,v in dic.items():
+            if v == count:
+                return k
+
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    bidders.append(name)
+    bids.append(bid)
+    if name in dic:
+        dic[name].append(bid)
+    else:
+        dic[name] = bid
+    
+    again = input("Are there any other bidders? Type 'Y' or 'N':  ").lower()
+    
+
+    if again == 'y':
+        info()
+    else:
+        count = 0
+        for i in dic.values():
+            if i > count:
+                count = i
+        print(f"The winner is {key(dic,count)} of ${count}.")
+
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         |_________|
+                         `'-------'`
+                       .-------------.
+                       |______________|
+
+'''
+
+print(logo)
+print("Welcome to Secret Auction Program")
+bidders = []
+bids = []
+dic = {}
+info()
+
+
 
