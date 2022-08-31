70c02cd2781ff6504634302654e6d336940ff209
manuu <admin@django.com>
2022-08-31 22:33:36 +0530

    Coffee Machine

--- Day_15/coffee_machine.py
+++ Day_15/coffee_machine.py
@@ -10,7 +10,7 @@ MENU = {
             "water": 150,
             "milk": 100,
             "coffee": 8
-        },"cost": 3
+        },"cost": 2.5
     },
     "cappuccino":{
         "ingredients":{
@@ -26,24 +26,39 @@ RESOURCES = {
     "coffee": 500,
 }
 
-def isenough_resources(order_ingredients):
+def isresource_sufficient(order_ingredients):
     for item in order_ingredients:
-        if order_ingredients[item] < RESOURCES[item]:
+        if order_ingredients[item] > RESOURCES[item]:
             print(f"Sorry, there is not enough {item}")
             return False
     return True
 
 def process_coins():
     print("Please insert coins. ")
-    quarters = float(input("How many quarters? ")) * 0.25
-    dimes = float(input("How many dimes? ")) * 0.10
-    nickles = float(input("How many nickles? ")) * 0.05
-    pennies = float(input("How many pennies? ")) * 0.01
-    return quarters+dimes+nickles+pennies
+    total = int(input("How many quarters? ")) * 0.25
+    total += int(input("How many dimes? ")) * 0.10
+    total += int(input("How many nickles? ")) * 0.05
+    total += int(input("How many pennies? ")) * 0.01
+    return total
 
-def is_transaction_successful(process, drinkcost):
+def is_transaction_successful(coins, drinkcost):
+    if coins >= drinkcost:
+        global profit
+        change = round(coins - drinkcost, 2)
+        print(f"Here is ${change} dollars in change")
+        profit += drinkcost
+        RESOURCES["Money"] = profit 
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded")
+        return False
 
-
+def make_coffee(choice, order_ingredients):
+    for i in order_ingredients:
+        RESOURCES[i] -= order_ingredients[i]
+    print(f"Here is your {choice}. Enjoy!")
+    
+profit = 0
 is_over = True
 while is_over:
     choice = input("What would you like? (espresso/latte/cappuccino/): ").lower()
@@ -53,12 +68,15 @@ while is_over:
         for k,v in RESOURCES.items():
             if k == "water" or k == "milk":
                 print(f"{k}: {v} ml")
+            elif k == "Money":
+                print(f"{k}: ${v}")
             else:
                 print(f"{k}: {v} g")
     else:
         drink = MENU[choice]
-        if isenough_resources(drink["ingredients"]):
-            is_transaction_successful(process_coins(), drink["cost"]):
-
+        if isresource_sufficient(drink["ingredients"]):
+            coins = process_coins()
+            if is_transaction_successful(coins, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
 
 
