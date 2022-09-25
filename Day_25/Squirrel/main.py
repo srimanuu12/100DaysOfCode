b5b9a20593d01ce6085140e0e5e5371488a680ce
srimanuu12 <hmanvitha@gmail.com>
2022-09-25 22:33:14 +0530

    List and Dictionary Comprehension

---
+++ Day_25/Squirrel/main.py
@@ -0,0 +1,15 @@
+from cgitb import grey
+import pandas
+
+data = pandas.read_csv("./100daysofcode/day_25/Squirrel/2018_centralpark_Squirrel_Data.csv")
+grey_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+squirrel = {
+    "Fur Color": ["Grey", "Cinnamon", "Black"],
+    "Count": [grey_count, red_count, black_count]
+}
+
+squirrel_data = pandas.DataFrame(squirrel)
+squirrel_data.to_csv("./100daysofcode/day_25/Squirrel/squirrel_count.csv")
